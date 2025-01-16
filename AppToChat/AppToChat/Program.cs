c9f45411845916007bcb3d8da34cf0a836718ef4
SkibidiSigma <szymon89098@gmail.com>
2025-01-16 22:10:34 +0100

    git test

--- AppToChat/AppToChat/Program.cs
+++ AppToChat/AppToChat/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+//test
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
