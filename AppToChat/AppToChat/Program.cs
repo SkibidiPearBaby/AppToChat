7a18ebf6c102650a0b675588cb413ac8ba8a8c11
SkibidiSigma <kolo21pl@gmail.com>
2025-01-16 22:13:37 +0100

    git test -> jetbrains rider

--- AppToChat/AppToChat/Program.cs
+++ AppToChat/AppToChat/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-//test
+//test me
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
